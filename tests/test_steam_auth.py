28d4e92af339bd97f4f320c871086f4b5f4ba81e
Gleb Gordeev <gleb.gordeev.a@gmail.com>
2024-06-16 19:35:35 +0300

    add test

--- tests/test_steam_auth.py
+++ tests/test_steam_auth.py
@@ -1,8 +1,18 @@
+from config import LINK
+from flows.common import HeaderFlows
+from flows.auth_page import AuthFlows
+from utils.utils import BaseActions as BaseA
+from containers.auth_page import LoginPage
+from config import STEAM_LOGIN, STEAM_PASSWORD
+
+ERRORS_LIST = ['Пожалуйста, проверьте свой пароль и имя аккаунта и попробуйте снова.',
+               'Please check your password and account name and try again.']
+
+
 def test_google_search(browser):
-    browser.get("https://store.steampowered.com/")
-
-    search_box = browser.find_element_by_name("q")
-    search_box.send_keys("Selenium")
-    search_box.submit()
-
-    assert "Selenium" in browser.page_source
+    browser.get(LINK)
+    HeaderFlows.click_to_login_button(browser)
+    AuthFlows.enter_account_name(browser, STEAM_LOGIN)
+    AuthFlows.enter_password(browser, STEAM_PASSWORD)
+    AuthFlows.click_login_btn(browser)
+    assert BaseA.find_element(browser, LoginPage.error_msg()).text in ERRORS_LIST
