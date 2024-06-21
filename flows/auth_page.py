30aaed6946484d7c937e2bb830b5c4aee2406206
Gleb Gordeev <gleb.gordeev.a@gmail.com>
2024-06-21 18:58:52 +0300

    Add BasePage with init

--- flows/auth_page.py
+++ flows/auth_page.py
@@ -1,16 +1,14 @@
 from selenium.webdriver.common.by import By
 from utils.wait_utils import WaitUtils
+from flows.base_page import BasePage
 
 
-class LoginPage:
+class LoginPage(BasePage):
     PIVOT_FORM_LOC = '//*[@data-featuretarget="login"]'
     NAME_FIELD_LOC = (By.XPATH, f'{PIVOT_FORM_LOC}//input[@type="text"]')
     PASS_FIELD_LOC = (By.XPATH, f'{PIVOT_FORM_LOC}//input[@type="password"]')
     LOGIN_BTN_LOC = (By.XPATH, f'{PIVOT_FORM_LOC}//button[@type="submit"]')
     ERROR_MSG_LOC = (By.XPATH, f'{PIVOT_FORM_LOC}//form/div[5]')
-
-    def __init__(self, browser):
-        self.browser = browser
 
     def enter_account_name(self, name):
         WaitUtils.wait_presence_of_element(self.browser, self.NAME_FIELD_LOC).send_keys(name)
--- flows/main_page.py
+++ flows/main_page.py
@@ -1,13 +1,11 @@
 from selenium.webdriver.common.by import By
 from utils.wait_utils import WaitUtils
+from flows.base_page import BasePage
 
 
-class MainPage:
+class MainPage(BasePage):
     PIVOT_FORM_LOC = '//*[@id="global_header"]'
     LOGIN_BUTTON_LOC = (By.XPATH, f'{PIVOT_FORM_LOC}//*[@id="global_action_menu"]/a[2]')
-
-    def __init__(self, browser):
-        self.browser = browser
 
     def click_to_login_button(self):
         WaitUtils.wait_presence_of_element(self.browser, self.LOGIN_BUTTON_LOC).click()
